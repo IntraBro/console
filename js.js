22ec27379f28a16d613d0f933a7e141c775c5c34
IntraBro <82090927+IntraBro@users.noreply.github.com>
2021-08-04 16:54:03 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+var fn = "Aditya"
+var ln = "Bhowmick"
+
+
+console.log(fn.concat(ln));
+var fno = 23
+var sno = 17
+console.log(fno/sno)
+console.log(fno*sno)
+console.log(fno+sno)
+console.log(fno-sno)
